ec549f0608761b9e22222abeb88c03bc28ad7c65
debora <debora.r.serra@gmail.com>
2022-07-18 20:42:46 -0300

    feat: finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { request, gql } from 'graphql-request';
-import { fetchedCurrencies } from '../features/currencies/currenciesSlice';
-import { connect } from 'react-redux';
+// import { fetchedCurrencies } from '../features/currencies/currenciesSlice';
+// import { connect } from 'react-redux';
 import logo from '../images/Brand-iconbrand_icon.svg';
-import cart from '../images/Empty-cart.svg';
+import cartImg from '../images/Empty-cart.svg';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currencies: [],
+      loading: true,
+      cart: 0,
+    }
+  }
   componentDidMount() {
     this.fetchCurrencies();
   }
 
   fetchCurrencies = async () => {
-    const { dispatch } = this.props;
     const query = gql`
       {
         currencies {
@@ -23,36 +30,47 @@ class Header extends React.Component {
     `
     const url = 'http://localhost:4000';
     const response = await request(url, query);
-    dispatch(fetchedCurrencies({ payload:  response.currencies }));
-    console.log({response});
+    this.setState((prevSt) => ({
+      ...prevSt,
+      currencies: response.currencies,
+      loading: false,
+    }))
   }
 
   render() {
-    const { currencies } = this.props;
-    console.log(currencies);
+    const { currencies, loading, cart } = this.state;
     return (
-      <header>
-        <nav>
-          <Link to="/women">WOMEN</Link>
-          <Link to="/men">MEN</Link>
-          <Link to="/kids">KIDS</Link>
-        </nav>
-        <img src={ logo } alt="Brand icon" />
-        <section>
-          <select name="currency">
-            {currencies?.map(({ label, symbol }) => (
-              <option key={ label } value={ label }>{symbol}</option>
-            ))}
-          </select>
-          <img src={ cart } alt="Cart icon" />
-        </section>
-      </header>
+      loading
+        ? <p>Loading</p>
+        : (
+          <header>
+            <nav>
+              <Link to="/women">WOMEN</Link>
+              <Link to="/men">MEN</Link>
+              <Link to="/kids">KIDS</Link>
+            </nav>
+            <img src={ logo } alt="Brand icon" />
+            <section>
+              <select name="currency">
+                {currencies?.map(({ label, symbol }) => (
+                  <option key={ label } value={ label }>{symbol}</option>
+                ))}
+              </select>
+              <section>
+                <img src={ cartImg } alt="Cart icon" />
+                {cart !== 0 && <section>{cart}</section>}
+              </section>
+            </section>
+          </header>
+        )
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  currencies: state.currencies,
-})
+// const mapStateToProps = (state) => ({
+//   currencies: state.currencies,
+// })
 
-export default connect(mapStateToProps)(Header);
+// export default connect(mapStateToProps)(Header);
+
+export default Header;
